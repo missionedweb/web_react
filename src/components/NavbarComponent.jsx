fe2b76e8d6f453c4d9b554fecf0b280cf30f30d7
subbhashit mukherjee <subbhashitmukherjee@gmail.com>
2020-09-04 20:33:12 +0530

    navbar problem

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -12,12 +12,15 @@ import { createStructuredSelector } from "reselect";
 import { selectCurrentUser } from "../redux/user/user.selector";
 import Register from "../pages/auth/register/register";
 
+
+
 function NavbarComponent({ currentUser }) {
   return (
     <>
+    
       <div id="header">
       <div class="row">
-      <Navbar center bg="dark" expand="lg">
+      <Navbar center bg="dark" expand="lg" className="navbar-expand-sm">
         <Navbar.Brand href="#home">
           <img className="logo mt-n4" src={NavigationLogo} alt="logo" />
           <span className="missioned-nav font-weight-bold">
@@ -25,7 +28,7 @@ function NavbarComponent({ currentUser }) {
           </span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav navbar-left">
           <Nav className="mr-auto mb-2">
 
             <ul>
@@ -36,7 +39,7 @@ function NavbarComponent({ currentUser }) {
               
       <li>
         <a href="">Classes</a>
-        <ul class="mega-dropdown">
+        <ul class="mega-dropdown mr-auto">
           <li class="row">
             <ul class="mega-col">
               <li><Nav.Link href="#action/3.1" className="link">Class 8</Nav.Link></li>
@@ -57,9 +60,9 @@ function NavbarComponent({ currentUser }) {
       </li>
       <li>
         <a href="">STUDY MATERIALS</a>
-        <ul class="mega-dropdown">
+        <ul class="mega-dropdown mr-auto">
           <li class="row">
-            <ul class="mega-col1">
+            <ul class="mega-col1 mr-auto">
               <li><Nav.Link href="#action/3.1" className="link">NEET</Nav.Link></li>
               <li><Nav.Link href="#action/3.1" className="link">NTSE</Nav.Link></li>
               <li><Nav.Link href="#action/3.1" className="link">CBSE</Nav.Link></li>
@@ -126,17 +129,17 @@ function NavbarComponent({ currentUser }) {
             
           </Nav>
           {currentUser !== null ? (
-            <Link to="/login">
-              <button className="btn login-btn mr-2 float-right" onClick={() => auth.signOut()}>
+            <Link to="/login" >
+              <button className="btn login-btn mr-auto float-right ml-auto" onClick={() => auth.signOut()}>
                 SignOut
               </button>
             </Link>
           ) : (
-            <Form inline>
-              <Link to="/login" className="btn login-btn mr-2 float-right" role="button">
+            <Form inline >
+              <Link to="/login" className="btn login-btn mr-2" role="button">
                 Login
               </Link>
-              <Register />
+              <Register className="mr-2" />
             </Form>
           )}
         </Navbar.Collapse>
