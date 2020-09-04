985bb7d6c8fdf5653de319c77343c446e6c92b66
subbhashit mukherjee <subbhashitmukherjee@gmail.com>
2020-09-04 16:45:01 +0530

    upgrade navbar

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -14,7 +14,9 @@ const NavbarComponent = () => {
   const { currentUser } = useContext(AuthContext);
   return (
     <>
-      <Navbar center  expand="lg" data-aos="zoom-out" data-aos-duration="1000">
+      <div id="header">
+      <div class="row">
+      <Navbar center bg="dark" expand="lg">
         <Navbar.Brand href="#home">
           <img className="logo mt-n4" src={NavigationLogo} alt="logo" />
           <span className="missioned-nav font-weight-bold">
@@ -24,12 +26,110 @@ const NavbarComponent = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <NavDropdown title="Classes" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Class 8</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Class 9</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Class 10</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">
+
+            <ul>
+              {/* <li>
+            <Nav.Link href="/" className="linkname">Home</Nav.Link>
+              </li> */}
+              <li>
+        <a href="/">Home</a>
+        
+      </li>
+              
+      <li>
+        <a href="">Classes</a>
+        <ul class="mega-dropdown">
+          <li class="row">
+            <ul class="mega-col">
+              <li><Nav.Link href="#action/3.1" className="link">Class 8</Nav.Link></li>
+              <li><Nav.Link href="#action/3.2" className="link">Class 9</Nav.Link></li>
+            </ul>
+            <ul class="mega-col">
+              <li><Nav.Link href="#action/3.3" className="link">Class 10</Nav.Link></li>
+              <li><Nav.Link href="#action/3.4" className="link">Class 11 Science</Nav.Link></li>
+              <li><Nav.Link href="#action/3.5" className="link">Class 12 Science</Nav.Link></li>
+            </ul>
+            <ul class="mega-col">
+              <li><Nav.Link href="#action/3.6" className="link">Class 11 Commerce</Nav.Link></li>
+              <li><Nav.Link href="#action/3.7" className="link">Class 12 Commerce</Nav.Link></li>
+            </ul>
+            
+          </li>
+        </ul>        
+      </li>
+      <li>
+        <a href="">STUDY MATERIALS</a>
+        <ul class="mega-dropdown">
+          <li class="row">
+            <ul class="mega-col1">
+              <li><Nav.Link href="#action/3.1" className="link">NEET</Nav.Link></li>
+              <li><Nav.Link href="#action/3.1" className="link">NTSE</Nav.Link></li>
+              <li><Nav.Link href="#action/3.1" className="link">CBSE</Nav.Link></li>
+              <li><Nav.Link href="#action/3.1" className="link">ICSE</Nav.Link></li>
+            </ul>
+            <ul class="mega-col1">
+              <li><Nav.Link href="#action/3.1" className="link">JEE MAINS</Nav.Link></li>
+              <li><Nav.Link href="#action/3.1" className="link">JEE ADVANCED</Nav.Link></li>
+              <li><Nav.Link href="#action/3.1" className="link">NCERT SOL</Nav.Link></li>
+              <li><Nav.Link href="#action/3.1" className="link">IMO</Nav.Link></li>
+            </ul>
+            <ul class="mega-col1">
+              <li><Nav.Link href="#action/3.1" className="link">KVPY</Nav.Link></li>
+              <li><Nav.Link href="#action/3.1" className="link">NSEJS</Nav.Link></li>
+              <li><Nav.Link href="#action/3.1" className="link">NSO</Nav.Link></li>
+              <li><Nav.Link href="#action/3.1" className="link">InPHO</Nav.Link></li>
+            </ul>
+            <ul class="mega-col1">
+              <li><Nav.Link href="#action/3.1" className="link">InCHO</Nav.Link></li>
+              <li><Nav.Link href="#action/3.1" className="link">RMO</Nav.Link></li>
+              <li><Nav.Link href="#action/3.1" className="link">NSO</Nav.Link></li>
+              <li><Nav.Link href="#action/3.1" className="link">NSTSE</Nav.Link></li>
+            </ul>
+            <ul class="mega-col1">
+              <li><Nav.Link href="#action/3.1" className="link">BITSAT</Nav.Link></li>
+              <li><Nav.Link href="#action/3.1" className="link">SRMJEE</Nav.Link></li>
+            </ul>
+            
+          </li>
+        </ul>        
+      </li>
+      <li>
+        <a href="">MORE</a>
+        <ul class="mega-dropdown">
+          <li class="row">
+            <ul class="mega-col1">
+              <li><Nav.Link href="#action/3.1" className="link">About Us</Nav.Link></li>
+            </ul>
+            <ul class="mega-col1">
+              <li><Nav.Link href="#action/3.1" className="link">Features</Nav.Link></li>
+            </ul>
+            <ul class="mega-col1">
+              <li><Nav.Link href="#action/3.1" className="link">Testimonials</Nav.Link></li>
+            </ul>
+            <ul class="mega-col1">
+              <li><Nav.Link href="#action/3.1" className="link">Counselling</Nav.Link></li>
+            </ul>
+            <ul class="mega-col1">
+              <li><Nav.Link href="#action/3.1" className="link">FAQ's</Nav.Link></li>
+            </ul>
+            
+          </li>
+        </ul>        
+      </li>
+      <li>
+      <a href="/pricing"> PRICING</a>
+      </li>
+      <li>
+      <a href="/https://missioned.in/blog/">BLOG</a>
+      </li>
+              
+    </ul> 
+            
+            {/* <NavDropdown title="Classes" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1" className="link">Class 8</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2" className="link">Class 9</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3" className="link">Class 10</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4" className="link">
                 Class 11 Science
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.5">
@@ -71,7 +171,7 @@ const NavbarComponent = () => {
               <NavDropdown.Item href="/faq">FAQ's</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="/pricing">Pricing</Nav.Link>
-            <Nav.Link href="https://missioned.in/blog/">Blog</Nav.Link>
+            <Nav.Link href="https://missioned.in/blog/">Blog</Nav.Link> */}
           </Nav>
           {currentUser !== null ? (
             <Link to="/dashboard">
@@ -98,6 +198,8 @@ const NavbarComponent = () => {
           )}
         </Navbar.Collapse>
       </Navbar>
+      </div>
+      </div>
     </>
   );
 };
