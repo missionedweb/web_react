5c426b76e5ce36ddde369816b11d77406265a9d7
shankha001 <shankhasubhrabiswas@gmail.com>
2020-09-05 09:27:51 +0530

    add and config login component

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -11,140 +11,251 @@ import { auth } from "../firebase/firebase.utils";
 import { createStructuredSelector } from "reselect";
 import { selectCurrentUser } from "../redux/user/user.selector";
 import Register from "../pages/auth/register/register";
-
-
+import Login from "../pages/auth/login/login";
 
 function NavbarComponent({ currentUser }) {
   return (
     <>
-    
       <div id="header">
-      <div class="row">
-      <Navbar center bg="dark" expand="lg" className="navbar-expand-sm">
-        <Navbar.Brand href="#home">
-          <img className="logo mt-n4" src={NavigationLogo} alt="logo" />
-          <span className="missioned-nav font-weight-bold">
-            Mission<span className="ed">Ed</span>
-          </span>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav navbar-left">
-          <Nav className="mr-auto mb-2">
+        <div class="row">
+          <Navbar center bg="dark" expand="lg" className="navbar-expand-sm">
+            <Navbar.Brand href="#home">
+              <img className="logo mt-n4" src={NavigationLogo} alt="logo" />
+              <span className="missioned-nav font-weight-bold">
+                Mission<span className="ed">Ed</span>
+              </span>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav navbar-left">
+              <Nav className="mr-auto mb-2">
+                <ul>
+                  <li>
+                    <a href="/">Home</a>
+                  </li>
 
-            <ul>
-              <li>
-        <a href="/">Home</a>
-        
-      </li>
-              
-      <li>
-        <a href="">Classes</a>
-        <ul class="mega-dropdown mr-auto">
-          <li class="row">
-            <ul class="mega-col">
-              <li><Nav.Link href="#action/3.1" className="link">Class 8</Nav.Link></li>
-              <li><Nav.Link href="#action/3.2" className="link">Class 9</Nav.Link></li>
-            </ul>
-            <ul class="mega-col">
-              <li><Nav.Link href="#action/3.3" className="link">Class 10</Nav.Link></li>
-              <li><Nav.Link href="#action/3.4" className="link">Class 11 Science</Nav.Link></li>
-              <li><Nav.Link href="#action/3.5" className="link">Class 12 Science</Nav.Link></li>
-            </ul>
-            <ul class="mega-col">
-              <li><Nav.Link href="#action/3.6" className="link">Class 11 Commerce</Nav.Link></li>
-              <li><Nav.Link href="#action/3.7" className="link">Class 12 Commerce</Nav.Link></li>
-            </ul>
-            
-          </li>
-        </ul>        
-      </li>
-      <li>
-        <a href="">STUDY MATERIALS</a>
-        <ul class="mega-dropdown mr-auto">
-          <li class="row">
-            <ul class="mega-col1 mr-auto">
-              <li><Nav.Link href="#action/3.1" className="link">NEET</Nav.Link></li>
-              <li><Nav.Link href="#action/3.1" className="link">NTSE</Nav.Link></li>
-              <li><Nav.Link href="#action/3.1" className="link">CBSE</Nav.Link></li>
-              <li><Nav.Link href="#action/3.1" className="link">ICSE</Nav.Link></li>
-            </ul>
-            <ul class="mega-col1">
-              <li><Nav.Link href="#action/3.1" className="link">JEE MAINS</Nav.Link></li>
-              <li><Nav.Link href="#action/3.1" className="link">JEE ADVANCED</Nav.Link></li>
-              <li><Nav.Link href="#action/3.1" className="link">NCERT SOL</Nav.Link></li>
-              <li><Nav.Link href="#action/3.1" className="link">IMO</Nav.Link></li>
-            </ul>
-            <ul class="mega-col1">
-              <li><Nav.Link href="#action/3.1" className="link">KVPY</Nav.Link></li>
-              <li><Nav.Link href="#action/3.1" className="link">NSEJS</Nav.Link></li>
-              <li><Nav.Link href="#action/3.1" className="link">NSO</Nav.Link></li>
-              <li><Nav.Link href="#action/3.1" className="link">InPHO</Nav.Link></li>
-            </ul>
-            <ul class="mega-col1">
-              <li><Nav.Link href="#action/3.1" className="link">InCHO</Nav.Link></li>
-              <li><Nav.Link href="#action/3.1" className="link">RMO</Nav.Link></li>
-              <li><Nav.Link href="#action/3.1" className="link">NSO</Nav.Link></li>
-              <li><Nav.Link href="#action/3.1" className="link">NSTSE</Nav.Link></li>
-            </ul>
-            <ul class="mega-col1">
-              <li><Nav.Link href="#action/3.1" className="link">BITSAT</Nav.Link></li>
-              <li><Nav.Link href="#action/3.1" className="link">SRMJEE</Nav.Link></li>
-            </ul>
-            
-          </li>
-        </ul>        
-      </li>
-      <li>
-        <a href="">MORE</a>
-        <ul class="mega-dropdown">
-          <li class="row">
-            <ul class="mega-col1">
-              <li><Nav.Link href="#action/3.1" className="link">About Us</Nav.Link></li>
-            </ul>
-            <ul class="mega-col1">
-              <li><Nav.Link href="#action/3.1" className="link">Features</Nav.Link></li>
-            </ul>
-            <ul class="mega-col1">
-              <li><Nav.Link href="#action/3.1" className="link">Testimonials</Nav.Link></li>
-            </ul>
-            <ul class="mega-col1">
-              <li><Nav.Link href="#action/3.1" className="link">Counselling</Nav.Link></li>
-            </ul>
-            <ul class="mega-col1">
-              <li><Nav.Link href="#action/3.1" className="link">FAQ's</Nav.Link></li>
-            </ul>
-            
-          </li>
-        </ul>        
-      </li>
-      <li>
-      <a href="/pricing"> PRICING</a>
-      </li>
-      <li>
-      <a href="/https://missioned.in/blog/">BLOG</a>
-      </li>
-              
-    </ul> 
-            
-            
-          </Nav>
-          {currentUser !== null ? (
-            <Link to="/login" >
-              <button className="btn login-btn mr-auto float-right ml-auto" onClick={() => auth.signOut()}>
-                SignOut
-              </button>
-            </Link>
-          ) : (
-            <Form inline >
-              <Link to="/login" className="btn login-btn mr-2" role="button">
-                Login
-              </Link>
-              <Register className="mr-2" />
-            </Form>
-          )}
-        </Navbar.Collapse>
-      </Navbar>
-      </div>
+                  <li>
+                    <a href="">Classes</a>
+                    <ul class="mega-dropdown mr-auto">
+                      <li class="row">
+                        <ul class="mega-col">
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              Class 8
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.2" className="link">
+                              Class 9
+                            </Nav.Link>
+                          </li>
+                        </ul>
+                        <ul class="mega-col">
+                          <li>
+                            <Nav.Link href="#action/3.3" className="link">
+                              Class 10
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.4" className="link">
+                              Class 11 Science
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.5" className="link">
+                              Class 12 Science
+                            </Nav.Link>
+                          </li>
+                        </ul>
+                        <ul class="mega-col">
+                          <li>
+                            <Nav.Link href="#action/3.6" className="link">
+                              Class 11 Commerce
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.7" className="link">
+                              Class 12 Commerce
+                            </Nav.Link>
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="">STUDY MATERIALS</a>
+                    <ul class="mega-dropdown mr-auto">
+                      <li class="row">
+                        <ul class="mega-col1 mr-auto">
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              NEET
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              NTSE
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              CBSE
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              ICSE
+                            </Nav.Link>
+                          </li>
+                        </ul>
+                        <ul class="mega-col1">
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              JEE MAINS
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              JEE ADVANCED
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              NCERT SOL
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              IMO
+                            </Nav.Link>
+                          </li>
+                        </ul>
+                        <ul class="mega-col1">
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              KVPY
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              NSEJS
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              NSO
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              InPHO
+                            </Nav.Link>
+                          </li>
+                        </ul>
+                        <ul class="mega-col1">
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              InCHO
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              RMO
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              NSO
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              NSTSE
+                            </Nav.Link>
+                          </li>
+                        </ul>
+                        <ul class="mega-col1">
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              BITSAT
+                            </Nav.Link>
+                          </li>
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              SRMJEE
+                            </Nav.Link>
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="">MORE</a>
+                    <ul class="mega-dropdown">
+                      <li class="row">
+                        <ul class="mega-col1">
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              About Us
+                            </Nav.Link>
+                          </li>
+                        </ul>
+                        <ul class="mega-col1">
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              Features
+                            </Nav.Link>
+                          </li>
+                        </ul>
+                        <ul class="mega-col1">
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              Testimonials
+                            </Nav.Link>
+                          </li>
+                        </ul>
+                        <ul class="mega-col1">
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              Counselling
+                            </Nav.Link>
+                          </li>
+                        </ul>
+                        <ul class="mega-col1">
+                          <li>
+                            <Nav.Link href="#action/3.1" className="link">
+                              FAQ's
+                            </Nav.Link>
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="/pricing"> PRICING</a>
+                  </li>
+                  <li>
+                    <a href="/https://missioned.in/blog/">BLOG</a>
+                  </li>
+                </ul>
+              </Nav>
+              {currentUser !== null ? (
+                <Link to="/" className="btn login-btn mr-2" role="button">
+                  <button
+                    className="btn login-btn mr-auto float-right ml-auto"
+                    onClick={() => auth.signOut()}>
+                    SignOut
+                  </button>
+                </Link>
+              ) : (
+                <Form inline>
+                  <Login className="mr-2" />
+
+                  <Register className="mr-2" />
+                </Form>
+              )}
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
       </div>
     </>
   );
--- src/pages/auth/login/login.jsx
+++ src/pages/auth/login/login.jsx
@@ -1,8 +1,102 @@
 import React, { useState } from "react";
+// import "./register.styles.scss";
+//Firebase
 import { auth } from "../../../firebase/firebase.utils";
-import "./login.styles.scss";
+//MaterialUI+Spring
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
-function Login({ history }) {
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import { useSpring, animated } from "react-spring/web.cjs"; // web.cjs is required for IE 11 support
+import Button from "@material-ui/core/Button";
+import logo from "../../../Images/logo.svg";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  button: {},
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "scroll",
+    margin: theme.spacing(2, 0, 2),
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "1px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(4, 4),
+    height: "100%",
+    [theme.breakpoints.down("lg")]: {
+      width: "30vw",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "80vw",
+    },
+    borderRadius: "5px",
+  },
+
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    padding: theme.spacing(1.5, 0),
+    fontSize: "16px",
+  },
+}));
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+  const { in: open, children, onEnter, onExited, ...other } = props;
+  const style = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    onStart: () => {
+      if (open && onEnter) {
+        onEnter();
+      }
+    },
+    onRest: () => {
+      if (!open && onExited) {
+        onExited();
+      }
+    },
+  });
+
+  return (
+    <animated.div ref={ref} style={style} {...other}>
+      {children}
+    </animated.div>
+  );
+});
+
+Fade.propTypes = {
+  children: PropTypes.element,
+  in: PropTypes.bool.isRequired,
+  onEnter: PropTypes.func,
+  onExited: PropTypes.func,
+};
+
+export default function Login() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const [details, setDetails] = useState({
     email: "",
     password: "",
@@ -19,7 +113,6 @@ function Login({ history }) {
     try {
       await auth.signInWithEmailAndPassword(email, password);
       setDetails({ email: "", password: "" });
-      history.push("/dashboard");
     } catch (err) {
       //error handling
       console.log(err.message);
@@ -27,32 +120,85 @@ function Login({ history }) {
   };
 
   return (
-    <React.Fragment>
-      <form
-        onSubmit={handleSubmit}
-        style={{ display: "flex", flexDirection: "column", width: "20%" }}>
-        <input
-          type="email"
-          onChange={handleChange}
-          required
-          id="email"
-          placeholder="Email Address"
-          name="email"
-          value={details.email}
-        />
-        <input
-          onChange={handleChange}
-          required
-          name="password"
-          placeholder="Password"
-          type="password"
-          id="password"
-          value={details.password}
-        />
-        <button type="submit">Login</button>
-      </form>
-    </React.Fragment>
+    <div>
+      <Button
+        variant="contained"
+        color="secondary"
+        type="button"
+        onClick={handleOpen}
+        className={classes.button}
+        style={{ outline: " none", marginRight: "10px" }}>
+        Login
+      </Button>
+
+      <Modal
+        aria-labelledby="spring-modal-title"
+        aria-describedby="spring-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}>
+        <Fade in={open}>
+          <div>
+            <form onSubmit={handleSubmit} className={classes.paper} noValidate>
+              <Grid container spacing={2}>
+                <Grid item xs={12} style={{ textAlign: "center" }}>
+                  <img src={logo} alt="logo" />
+                </Grid>
+                <Grid item xs={12} style={{ textAlign: "center" }}>
+                  <h2>Welcome Back !</h2>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    onChange={handleChange}
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    value={details.email}
+                    autoComplete="email"
+                    autoFocus
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    onChange={handleChange}
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    value={details.password}
+                    autoComplete="current-password"
+                  />
+                </Grid>
+              </Grid>
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}>
+                Sign In
+              </Button>
+            </form>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
   );
 }
-
-export default Login;
