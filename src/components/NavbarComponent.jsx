329831dc59cb963b69c8efdb233acf5236d02024
*Akshay <aj.18052000@gmail.com>
2020-09-13 18:27:29 +0530

    Landing Page Updated

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -18,7 +18,7 @@ import $ from 'jquery';
 function NavbarComponent({ currentUser }) {
   window.addEventListener("scroll", function () {
     let header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
+    
   });
   return (
     <>
