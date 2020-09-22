6ecfec7459d151dc8281f3f4e8d15ba9d4c4a484
shankha001 <shankhasubhrabiswas@gmail.com>
2020-09-22 11:36:46 +0530

    map only enrolled courses

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -3,24 +3,42 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Grid } from "@material-ui/core";
 import Card from "./CardTemplate";
 import { firestore } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCurrentUser } from "../../redux/user/user.selector";
 
-export default function CenteredGrid() {
+function CenteredGrid({ currentUser }) {
   const [courses, setCourses] = useState([]);
+  var userId;
+  if (currentUser) {
+    userId = currentUser.id;
+  }
 
   const classes = useStyles();
   useEffect(() => {
-    firestore.collection("course").onSnapshot((snapshot) => {
-      setCourses(
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          title: doc.data().title,
-        }))
-      );
-    });
-  }, []);
+    var courseRef = firestore.collection("course");
+    //query if course contains auth user.
+    var query = courseRef.where("users", "array-contains", `${userId}`);
+    query
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          setCourses((course) => [
+            ...course,
+            {
+              id: doc.id,
+              title: doc.data().title,
+            },
+          ]);
+        });
+      })
+      .catch(function (error) {
+        console.log("Error getting documents: ", error);
+      });
+  }, [userId]);
+
   return (
     <div className={classes.root}>
-      {console.log(courses)}
       <Grid container spacing={3}>
         {courses &&
           courses.map((course) => (
@@ -29,40 +47,18 @@ export default function CenteredGrid() {
                 id={course.id}
                 title={course.title}
                 les="200"
-                // image1="url('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png')"
                 image1={require("../Images/image20.png")}
                 time="2h 30min"
                 avatarimg="https://img.cinemablend.com/filter:scale/quill/0/7/9/6/5/6/079656ce9e00f1ef328bb6c1a41958b4c0ceb6cb.jpg?mw=600"
               />
             </Grid>
           ))}
-
-        {
-          //   <Grid item xs={12} sm={6} className={classes.card}>
-          //   <Card
-          //     subject="Physics"
-          //     les="200"
-          //     image1={require("../Images/image23.png")}
-          //     time="2h 30min"
-          //     avatarimg="https://img.cinemablend.com/filter:scale/quill/0/7/9/6/5/6/079656ce9e00f1ef328bb6c1a41958b4c0ceb6cb.jpg?mw=600"
-          //   />
-          // </Grid>
-          // <Grid item xs={12} sm={6} className={classes.card}>
-          //   <Card
-          //     subject="Biology"
-          //     les="200"
-          //     image1={require("../Images/image24.png")}
-          //     time="2h 30min"
-          //     avatarimg="https://img.cinemablend.com/filter:scale/quill/0/7/9/6/5/6/079656ce9e00f1ef328bb6c1a41958b4c0ceb6cb.jpg?mw=600"
-          //   />
-          // </Grid>
-        }
       </Grid>
     </div>
   );
 }
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     flexGrow: 1,
   },
@@ -70,3 +66,9 @@ const useStyles = makeStyles((theme) => ({
     minWidth: 275,
   },
 }));
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+});
+
+export default connect(mapStateToProps, null)(CenteredGrid);
