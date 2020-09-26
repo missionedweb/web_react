658577632745088cb1932dcec0b5743c5d57089b
shankha001 <shankhasubhrabiswas@gmail.com>
2020-09-26 23:33:15 +0530

    add dash navbar

---
+++ src/components/DashNavbar/dashnavbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./dashnavbar.styles.scss";
+import Avatar from "@material-ui/core/Avatar";
+function DashNavbar() {
+  return (
+    <React.Fragment>
+      <div className="dashnav__main">
+        {" "}
+        <Avatar>N</Avatar>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default DashNavbar;
