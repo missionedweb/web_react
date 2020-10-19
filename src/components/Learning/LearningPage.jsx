ec7a890aa0f988480a9a978b951d9d9b6de1da74
patelaryan7751 <59426397+patelaryan7751@users.noreply.github.com>
2020-10-19 06:18:05 +0530

    illustraion now visible in all sizes issue fixed

--- src/components/Learning/LearningPage.jsx
+++ src/components/Learning/LearningPage.jsx
@@ -66,15 +66,18 @@ const LearningPage = () => {
         <br />
         <br />
         <br />
-        
+          <div className="row">
+        <div className="col-md-6">
         <img
-          className="img-section-left"
+          className="img-fluid"
           data-aos="fade-right"
           data-aos-duration="1500"
           src={Left}
           alt=""
-          style={{ marginTop: "20px", width: "42%" }}
+         
         />
+              </div>
+              <div className="col-md-6">
         <div className="learning-page " data-aos="fade-left" data-aos-duration="1500">
           <p className="learning-heading">A new way to explore the learning</p>
           <h6 className="learning-txt">
@@ -86,6 +89,7 @@ const LearningPage = () => {
           </h6>
           <br />
           <br />
+                  </div>
           <div className="col-md-6" data-aos="zoom-in-up" data-aos-duration="1500">
             {" "}
             <a href="/feature1">
@@ -98,14 +102,17 @@ const LearningPage = () => {
         <br />
         <br />
         {/* Learning Page 2  */}
+                        <div className="col-md-6" >
         <img
-          className="img-section-right"
+          className="img-fluid"
           data-aos="fade-left"
           data-aos-duration="2500"
           src={Right}
           alt=""
-          style={{ marginTop: "20px", width: "45%" }}
+         
         />
+              </div>
+               <div className="col-md-6" >
         <div className="learning-page-2" data-aos="fade-left" data-aos-duration="1500">
           <p className="learning-heading-2">Let’s fall in love with learning</p>
           <h6 className="learning-txt-2">
@@ -115,6 +122,7 @@ const LearningPage = () => {
           </h6>
           <br />
           <br />
+                   </div>
           <div className="col-md-6" data-aos="zoom-in-up" data-aos-duration="1500">
             {" "}
             <a type="button" href="/feature2">
@@ -125,6 +133,7 @@ const LearningPage = () => {
         
       </div>
     </div>
+              </div>
   );
 };
 
