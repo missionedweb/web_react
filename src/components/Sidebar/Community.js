11217ee0effaaedd0834760ea2937dd19f40df3d
atharv123-wq <atharvamaid@gmail.com>
2020-10-30 16:02:55 +0530

    updated community page , made responsive

--- src/components/Sidebar/Community.js
+++ src/components/Sidebar/Community.js
@@ -3,6 +3,7 @@ import {Search, Create, ThumbUpAlt, Comment} from '@material-ui/icons';
 import Chip from '@material-ui/core/Chip';
 import {Card} from 'react-bootstrap';
 import {Button} from '@material-ui/core';
+import '../css/cards.scss';
  const Community = () =>{
     return(
         <div>
@@ -14,28 +15,29 @@ import {Button} from '@material-ui/core';
                     <input className="form-control" placeholder="Search " name="search"/>
                     </div>
                     <div className="col-lg-7">
-                        <button className="btn mx-3 active" style={{color:"#5E81F4"}}>Discuss</button>
+                        <button className="btn mx-3 " style={{color:"#2857f0"}}>Discuss</button>
                         <button className="btn mx-3" style={{color:"#5E81F4"}}>Problems</button>
                     </div>
                 </div>
                 <br/>
                 <br/>
-                <div className="row">
-                    <div className="col-lg-7" style={{display:"flex"}}>
+                <div className="row" style={{maxWidth:"100% !important"}}>
+                    <div className="col-lg-7 " style={{display:"flex", maxWidth:"100%"}}>
                         <Create style={{color:"#5E81F4"}} className="mt-2"/>
                         <input className="form-control mx-2" placeholder="Write Something" style={{height:"50px"}}/>
-                        <Chip color="primary" className="mt-2 mx-2" icon={<Search/>} label="Find posts by tags" />
                     </div>
                     <div className="col-lg-5">
+                    <Chip color="primary" className="mt-2 mx-2" icon={<Search/>} label="Find posts by tags" />
 
                     </div>
                 </div>
+                
                 <br/>
                 <br/>
-                <Card className="my-2">
+                <Card style={{maxWidth:"100% !important"}} className="my-3">
                 <Card.Header className="bg-light" as="h5">Prof. posted Google L3 | Microsoft L61 | Goldman Sachs</Card.Header>
                 <Card.Body>
-                    <Card.Title>Special title treatment</Card.Title>
+                  
                     <Card.Text>
                     With supporting text below as a natural lead-in to additional content.
                     </Card.Text>
@@ -43,10 +45,21 @@ import {Button} from '@material-ui/core';
                     <Button color="primary" startIcon={<Comment />} className="mx-2">Comment</Button>
                 </Card.Body>
                 </Card>
-                <Card className="my-2">
+                <Card style={{maxWidth:"100% !important"}} className="my-3">
                 <Card.Header className="bg-light" as="h5">Prof. posted Google L3 | Microsoft L61 | Goldman Sachs</Card.Header>
                 <Card.Body>
-                    <Card.Title>Special title treatment</Card.Title>
+                   
+                    <Card.Text>
+                    With supporting text below as a natural lead-in to additional content.
+                    </Card.Text>
+                    <Button startIcon={<ThumbUpAlt/>} color="primary" className="mx-2">Like</Button>
+                    <Button startIcon={<Comment />} color="primary" className="mx-2">Comment</Button>
+                </Card.Body>
+                </Card>
+                <Card style={{maxWidth:"100% !important"}} className="my-3">
+                <Card.Header className="bg-light" as="h5">Prof. posted Google L3 | Microsoft L61 | Goldman Sachs</Card.Header>
+                <Card.Body>
+                   
                     <Card.Text>
                     With supporting text below as a natural lead-in to additional content.
                     </Card.Text>
